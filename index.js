d807364f5941be3e64b0936b028211bbc4105754
Walter Santos Filho <wcsantosfilho@gmail.com>
2021-02-25 14:23:57 -0300

    Listening

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Datastore = require('nedb');
 var db = new Datastore( { inMemoryOnly: true });
 var ODataServer = require('simple-odata-server');
 var Adapter = require('simple-odata-server-nedb');
+const PORT = process.env.PORT || 5000
  
 var model = {
     namespace: "jsreport",
@@ -25,4 +26,4 @@ var odataServer = ODataServer("https://vast-caverns-44119.herokuapp.com:5000")
     .adapter(Adapter(function(es, cb) { cb(null, db)}));
  
  
-http.createServer(odataServer.handle.bind(odataServer)).listen(5000);
+http.createServer(odataServer.handle.bind(odataServer)).listen(PORT, () => console.log(`Listening on ${PORT}`));
